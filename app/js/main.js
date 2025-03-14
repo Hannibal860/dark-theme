105a0104834faf145152c9509c7b7131a7d5ba83
Hannibal860 <skorokhodeugene@gmail.com>
2025-03-14 09:12:13 -0600

    improved dark theme

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,25 @@
 const themeBtn = document.querySelector('.theme-btn');
 const themeValue = document.querySelector('.theme-btn__default');
 const body = document.querySelector('body');
+const logo = document.querySelector('.logo__img');
 
 themeBtn.addEventListener('click', switchTheme);
-function switchTheme (e) {
+function switchTheme(e) {
   themeValue.classList.toggle('not-default');
   body.classList.toggle('dark');
+  themeBtn.classList.toggle('dark');
+  logo.classList.toggle('dark');
+  if (themeValue.classList.contains('not-default')) {
+    themeValue.innerHTML = `<svg class="icon" width="24" height="24">
+      <use xlink:href="images/icons/sprite.svg#icon-moonlight"></use>
+    </svg>`  } else {
+    themeValue.innerHTML = `<svg class="icon" width="24" height="24">
+      <use xlink:href="images/icons/sprite.svg#icon-sun"></use>
+      </svg>`
+  };
+  if (logo.classList.contains('dark')) {
+    logo.setAttribute("src", "./images/icons/logo-white.svg")
+  } else {
+    logo.setAttribute("src", "./images/icons/logo.svg")
+  }  
 }
